d9cd674cf84923d07f7d2b0b0320be67b4d40a22
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-02-18 23:15:23 +0700

    add error icon

--- src/layout/heroText.jsx
+++ src/layout/heroText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const heroText = ({children}) => {
   return (
-    <div className='lg:p-28 md:p-12 p-6 leading-7 '>{children}</div>
+    <div className='lg:p-28 md:p-12 p-6 leading-7'>{children}</div>
   )
 }
 
