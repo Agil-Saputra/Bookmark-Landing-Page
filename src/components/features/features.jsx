709c7a429bf83848022831e6248b0c76bcb79f97
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-02-21 22:18:37 +0700

    add tabs hover effect

--- src/components/features/features.jsx
+++ src/components/features/features.jsx
@@ -1,13 +1,15 @@
+// import all components
 import { useState } from "react";
 import TabContent from "./tabContent";
 import TabNav from "./tabNav";
 
+// import all images for tabContent
 import tab1 from "../../assets/images/illustration-features-tab-1.svg";
 import tab2 from "../../assets/images/illustration-features-tab-2.svg";
 import tab3 from "../../assets/images/illustration-features-tab-3.svg";
 
-
 const features = () => {
+  // set default usestate
   const [activeTab, setActiveTab] = useState("1");
 
   return (
@@ -21,15 +23,48 @@ const features = () => {
         </p>
       </div>
 
-      <nav className="flex max-md:flex-col justify-center text-center mx-8 mt-8">
-      <TabNav id="1" title="Simple Bookmarking" setActiveTab={setActiveTab} activeTab={activeTab}/>
-      <TabNav id="2" title="Speedy Searching" setActiveTab={setActiveTab} activeTab={activeTab}/>
-      <TabNav id="3" title="Easy Sharing" setActiveTab={setActiveTab} activeTab={activeTab}/>
+      {/* tabnav for navigating between panel, change the active states to id using useState hooks*/}
+      <nav className="flex max-md:flex-col justify-center text-center mx-8 mt-8 smooth">
+        <TabNav
+          id="1"
+          title="Simple Bookmarking"
+          setActiveTab={setActiveTab}
+          activeTab={activeTab}
+        />
+        <TabNav
+          id="2"
+          title="Speedy Searching"
+          setActiveTab={setActiveTab}
+          activeTab={activeTab}
+        />
+        <TabNav
+          id="3"
+          title="Easy Sharing"
+          setActiveTab={setActiveTab}
+          activeTab={activeTab}
+        />
       </nav>
+
+      {/* tabcontent get the id and activetab states to render a current tabpanel*/}
       <div>
-        <TabContent id='1' activeTab={activeTab} title="Bookmark in one click" img={tab1}/>
-        <TabContent id='2' activeTab={activeTab} title="Intelligent search" img={tab2}/>
-        <TabContent id='3' activeTab={activeTab} title="Share your bookmarks" img={tab3}/>
+        <TabContent
+          id="1"
+          activeTab={activeTab}
+          title="Bookmark in one click"
+          img={tab1}
+        />
+        <TabContent
+          id="2"
+          activeTab={activeTab}
+          title="Intelligent search"
+          img={tab2}
+        />
+        <TabContent
+          id="3"
+          activeTab={activeTab}
+          title="Share your bookmarks"
+          img={tab3}
+        />
       </div>
     </div>
   );
--- src/layout/layout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const layout = ({children}) => {
-  return (
-    <div className='max-md:px-6'>{children}</div>
-  )
-}
-
-export default layout
